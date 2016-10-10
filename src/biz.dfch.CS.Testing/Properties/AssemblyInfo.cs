77926522b6c80926bf2b9b3f5a20512648178733
Ronald Rink <ronald.rink@d-fens.net>
2016-10-10 07:38:14 +0200

    InternalsVisibleTo
    * fixed wrong keyword in annotation (PublicKey , was PublicKeyToken)

--- src/biz.dfch.CS.Testing/Properties/AssemblyInfo.cs
+++ src/biz.dfch.CS.Testing/Properties/AssemblyInfo.cs
@@ -32,10 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.*")]
+[assembly: AssemblyVersion("1.1.1.*")]
 
 [assembly: InternalsVisibleTo("biz.dfch.CS.Testing.Tests" +
-",PublicKeyToken=" +
+",PublicKey=" +
 "00240000048000009400000006020000002400005253413100040000010001001b40be1e0fe173" +
 "6d8c862c055965cb78086432e7b2377bc9c36122936b394e789fcdfbfeecebbc1d7f90b0be7fd4" +
 "f871822cc3a06d5af4ff4d6a9fa0176b9b3fa53e17c407adeb1caec47ee243b213eff6fdb57438" +
